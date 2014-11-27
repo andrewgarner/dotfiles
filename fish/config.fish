2b009bc7878622bd894d2db00eefdff3cc851942
Andrew Garner <andrew@andrewgarner.com>
2014-11-27 08:01:22 +0000

    Add $GOPATH

--- fish/config.fish
+++ fish/config.fish
@@ -12,6 +12,8 @@ set -x DOCKER_HOST tcp://192.168.59.103:2375
 set -x DOCKER_CERT_PATH $HOME/.boot2docker/certs/boot2docker-vm
 set -x DOCKER_TLS_VERIFY 1
 
+set -x GOPATH $HOME/.go
+
 nodenv rehash >/dev/null ^&1
 rbenv rehash >/dev/null ^&1
 
