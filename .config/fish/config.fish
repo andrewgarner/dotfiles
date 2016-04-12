c06636b2343929e175e6b7110edf9b190502c2ee
Andrew Garner <andrew@andrewgarner.com>
2016-04-12 13:13:57 +0100

    No longer using boot2docker

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,10 +10,6 @@ set -x NODE_BUILD_CACHE_PATH $HOME/.nodenv/cache
 set -x RUBY_BUILD_CACHE_PATH $HOME/.rbenv/cache
 set -x RUBY_CONFIGURE_OPTS --with-readline-dir=(brew --prefix readline)
 
-set -x DOCKER_HOST tcp://192.168.59.103:2375
-set -x DOCKER_CERT_PATH $HOME/.boot2docker/certs/boot2docker-vm
-set -x DOCKER_TLS_VERIFY 1
-
 set -x GOPATH $HOME/.go
 
 if not begin
