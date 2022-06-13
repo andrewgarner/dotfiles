eeddfa63d6e157db98f2e4aa335da5db01c77568
Andrew Garner <andrew.garner@cazoo.co.uk>
2022-06-13 18:58:59 +0100

    Disable Homebrew cleanup hints

--- fish/.config/fish/conf.d/01_brew.fish
+++ fish/.config/fish/conf.d/01_brew.fish
@@ -1,4 +1,6 @@
 set -g fish_user_paths "/opt/homebrew/bin" $fish_user_paths
 set -g fish_user_paths "/opt/homebrew/sbin" $fish_user_paths
 
+set -x HOMEBREW_NO_INSTALL_CLEANUP true
+
 set -u WHALEBREW_INSTALL_PATH /opt/homebrew/bin
